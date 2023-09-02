42523b00f550cc3afc4d19b5144246434e170b2f
Pasquale Riello <pasqualeriello19@hotmail.it>
2023-09-02 11:56:13 +0200

    commit

--- T7-G31/RemoteCCC/src/main/java/RemoteCCC/App/App.java
+++ T7-G31/RemoteCCC/src/main/java/RemoteCCC/App/App.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.io.BufferedReader;
 import java.io.File;
 
 import java.io.FileWriter;
@@ -134,6 +136,11 @@ public class App {
         processBuilder.directory(new File(Config.getpathCompiler()));
     
         Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null)
+            System.out.println("tasklist: " + line);
         int exitCode = process.waitFor();
        
         // Legge il contenuto del buffer del terminale
